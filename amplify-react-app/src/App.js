55ff6868c58c86e04713969d2a1400c165cc2f73
Jon Newton <jon.newton@gmail.com>
2020-12-21 11:48:44 -0700

    update UI with start, limit inputs

--- amplify-react-app/src/App.js
+++ amplify-react-app/src/App.js
@@ -1,21 +1,32 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {API} from 'aws-amplify';
 import './App.css';
 
 function App() {
   const [coins, updateCoins] = useState([]);
+  const [input, updateInput] = useState({limit: 5, start: 0})
+
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value });
+  }
 
   async function fetchCoins() {
-    const data = await API.get('cryptoapi', '/coins');
+    const {limit, start} = input;
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
     updateCoins(data.coins);
   }
 
-  useEffect(() => {
-    fetchCoins()
-  }, [])
-
   return (
     <div className="App">
+      <input
+        onChange={e => updateInputValues('limit', e.target.value)}
+        placeholder="limit"
+      />
+      <input
+        placeholder="start"
+        onChange={e => updateInputValues('start', e.target.value)}
+      />
+      <button onClick={fetchCoins}>Fetch Coins</button>
       {
         coins.map((coin, index) => (
           <div key={index}>
