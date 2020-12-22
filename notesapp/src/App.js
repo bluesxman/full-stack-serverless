28667d3617b1212e2544a3741aba8901f27dd57f
Jon Newton <jon.newton@gmail.com>
2020-12-22 11:35:53 -0700

    add create note

--- notesapp/src/App.js
+++ notesapp/src/App.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useReducer} from 'react'
 import { API } from 'aws-amplify'
-import { List } from 'antd'
 import 'antd/dist/antd.css'
+import { v4 as uuid } from 'uuid'
+import { List, Input, Button } from 'antd'
 import { listNotes } from './graphql/queries'
+import { createNote as CreateNote } from "./graphql/mutations";
 
-// import logo from './logo.svg';
-// import './App.css';
+const CLIENT_ID = uuid()
 
 const initialState = {
   notes: [],
@@ -18,6 +19,12 @@ function reducer(state, action) {
   switch (action.type) {
     case 'SET_NOTES':
       return { ...state, notes: action.notes, loading: false }
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes]}
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form }
+    case 'SET_INPUT':
+      return { ...state, form: { ...state.form, [action.name]: action.value }}
     case 'ERROR':
       return { ...state, loading: false, error: true }
     default:
@@ -40,6 +47,29 @@ function App() {
     }
   }
 
+  async function createNote() {
+    const { form } = state
+    if (!form.name || !form.description) {
+      return alert('please enter a name and description')
+    }
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() }
+    dispatch({ type: 'ADD_NOTE', note })
+    dispatch({type: 'RESET_FORM' })
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: {input: note}
+      })
+      console.log('successfully created note')
+    } catch (err) {
+      console.log(`error: ${err}`)
+    }
+  }
+
+  function onChange(e) {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value})
+  }
+
   useEffect(() => {
     fetchNotes()
   }, [])
@@ -52,7 +82,7 @@ function App() {
           description={item.description}
         />
       </List.Item>
-    )
+    );
   }
 
   const styles = {
@@ -64,6 +94,24 @@ function App() {
 
   return (
     <div style={styles.container}>
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder="Note Name"
+        name='name'
+        state={styles.input}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder="Note description"
+        name='description'
+        state={styles.input}
+      />
+      <Button
+        onClick={createNote}
+        type="primary"
+      >Create Note</Button>
       <List
         loading={state.loading}
         dataSource={state.notes}
